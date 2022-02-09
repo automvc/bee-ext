df98ce753ee44c3aea374cf4c3d27609a789fe2a
Kingstar <honeysoft@126.com>
2022-02-09 20:53:00 +0800

    update BeeExtRedisCache

--- src/main/java/org/teasoft/beex/cache/redis/BeeExtRedisCache.java
+++ src/main/java/org/teasoft/beex/cache/redis/BeeExtRedisCache.java
@@ -1,16 +1,22 @@
 /*
- * Copyright 2016-2021 the original author.All rights reserved.
+ * Copyright 2016-2022 the original author.All rights reserved.
  * Kingstar(honeysoft@126.com)
  * The license,see the LICENSE file.
  */
 
 package org.teasoft.beex.cache.redis;
 
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.teasoft.bee.osql.Serializer;
 import org.teasoft.honey.osql.core.DefaultBeeExtCache;
+import org.teasoft.honey.osql.core.HoneyConfig;
+import org.teasoft.honey.osql.core.JdkSerializer;
 import org.teasoft.honey.osql.core.Logger;
+import org.teasoft.honey.util.StringUtils;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Protocol;
 
 /**
  * @author Kingstar
@@ -18,29 +24,91 @@ import redis.clients.jedis.Jedis;
  */
 public class BeeExtRedisCache extends DefaultBeeExtCache {
 
-	Jedis jedis; //TODO
-	Serializer serializer = new JdkSerializer();
+	JedisPool jedisPool;
+	Jedis jedis;
+	Serializer serializer;
 
 	private static final String id = "_SYS_BEE_REDIS_CACHE";
 	private static final byte[] idBytes = id.getBytes();
 
+	public BeeExtRedisCache() {
+		initRedis();
+	}
+
+	public void initRedis() {
+		HoneyConfig config = HoneyConfig.getHoneyConfig();
+
+		String host = config.cacheRedis_host;
+		Integer port = config.cacheRedis_port;
+		String password = config.cacheRedis_password;
+		Integer connectionTimeout = config.cacheRedis_connectionTimeout;
+		Integer soTimeout = config.cacheRedis_soTimeout;
+		Integer database = config.cacheRedis_database;
+		String clientName = config.cacheRedis_clientName;
+		boolean ssl = config.cacheRedis_ssl;
+
+		if (StringUtils.isBlank(host)) {
+			host = Protocol.DEFAULT_HOST;
+		}
+		if (port == null) {
+			port = Protocol.DEFAULT_PORT;
+		}
+
+		if (connectionTimeout == null) {
+			connectionTimeout = Protocol.DEFAULT_TIMEOUT;
+		}
+		if (soTimeout == null) {
+			soTimeout = Protocol.DEFAULT_TIMEOUT;
+		}
+
+		if (database == null) {
+			database = Protocol.DEFAULT_DATABASE;
+		}
+
+		this.jedisPool = new JedisPool(new GenericObjectPoolConfig<Jedis>(), host, port,
+				connectionTimeout, soTimeout, password, database, clientName, ssl);
+	}
+
 	@Override
 	public Object getInExtCache(String key) {
-		Logger.info("doing in getInExtCache  for BeeExtRedisCache");
-
-		return serializer.unserialize(jedis.hget(idBytes, key.toString().getBytes()));
+		return getSerializer().unserialize(getJedis().hget(idBytes, key.toString().getBytes()));
 	}
 
 	@Override
 	public void addInExtCache(String key, Object result) {
-		Logger.info("doing in addInExtCache  for BeeExtRedisCache");
-		jedis.hset(idBytes, key.toString().getBytes(), serializer.serialize(result));
+		getJedis().hset(idBytes, key.toString().getBytes(), getSerializer().serialize(result));
 	}
 
 	@Override
 	public void clearInExtCache(String key) {
-		Logger.info("doing in clearInExtCache  for BeeExtRedisCache");
-		jedis.hdel(id, key.toString());
+		getJedis().hdel(id, key);
+	}
+
+	public JedisPool getJedisPool() {
+		return jedisPool;
+	}
+
+	public void setJedisPool(JedisPool jedisPool) {
+		this.jedisPool = jedisPool;
+	}
+
+	public Jedis getJedis() {
+		JedisPool pool = getJedisPool();
+		if (pool != null) return pool.getResource();
+		return jedis;
+	}
+
+	public void setJedis(Jedis jedis) {
+		this.jedis = jedis;
+	}
+
+	public Serializer getSerializer() {
+		if (serializer == null) return new JdkSerializer();
+		return serializer;
+	}
+
+	public void setSerializer(Serializer serializer) {
+		this.serializer = serializer;
 	}
 
 }
