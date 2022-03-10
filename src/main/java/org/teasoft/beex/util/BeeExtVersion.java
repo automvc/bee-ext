65c8f4e7df7ee14ca7db34979f5009ec27525563
Kingstar <honeysoft@126.com>
2022-03-10 10:51:32 +0800

    add BeeExtVersion

---
+++ src/main/java/org/teasoft/beex/util/BeeExtVersion.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2016-2022 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.beex.util;
+
+/**
+ * @author Kingstar
+ * @since  1.11
+ */
+public final class BeeExtVersion {
+	
+	private BeeExtVersion() {}
+
+	public static final String version = "V1.11";
+	public static final String buildId = "V1.11.0.3.12";
+
+}
