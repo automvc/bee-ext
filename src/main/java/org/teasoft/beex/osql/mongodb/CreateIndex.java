c0cf3199f49ed64daa8169d376b06d3f1d5d3421
Kingstar <honeysoft@126.com>
2023-03-26 22:43:05 +0800

    add Index, Geo

---
+++ src/main/java/org/teasoft/beex/osql/mongodb/CreateIndex.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2020-2023 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.teasoft.beex.osql.mongodb;
+
+import java.util.List;
+
+/**
+ * @author Kingstar
+ * @since  2.1
+ */
+public interface CreateIndex {
+
+	public String index(String collectionName, String fieldName, IndexType indexType);
+
+	public String unique(String collectionName, String fieldName, IndexType indexType);
+
+	public List<String> indexes(String collectionName, List<IndexPair> indexes);
+	
+	/**
+	 * Drop all the indexes on this collection, except for the default on _id.
+	 * @param collectionName collection name
+	 */
+	public void dropIndexes(String collectionName);  
+}
