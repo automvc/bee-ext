f5dce2fc4d2f69b0b03a148dabe2ea545a098d21
Kingstar <honeysoft@126.com>
2024-01-06 01:17:11 +0800

    update BeeExtVersion

--- src/main/java/org/teasoft/beex/util/BeeExtVersion.java
+++ src/main/java/org/teasoft/beex/util/BeeExtVersion.java
@@ -31,7 +31,7 @@ public final class BeeExtVersion {
 	private BeeExtVersion() {}
 
 	public static final String version = "2.2";
-	public static final String buildId = version + ".0.1";
+	public static final String buildId = version + ".0.6";
 	
 	static {
 		printVersion();
