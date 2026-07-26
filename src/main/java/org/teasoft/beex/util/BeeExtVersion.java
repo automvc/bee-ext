57f97466d46707f23e50ee9b596ee81a50dd59bd
Kingstar <honeysoft@126.com>
2026-07-27 02:07:35 +0800

    update for 3.0.0.7

--- src/main/java/org/teasoft/beex/util/BeeExtVersion.java
+++ src/main/java/org/teasoft/beex/util/BeeExtVersion.java
@@ -31,7 +31,7 @@ public final class BeeExtVersion {
 	private BeeExtVersion() {}
 
 	public static final String version = "3.0.0";
-	public static final String buildId = version + ".6";
+	public static final String buildId = version + ".7";
 
 	static {
 		printVersion();
